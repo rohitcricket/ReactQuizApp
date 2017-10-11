11a237a08fa13ffb503bba3fabc48893d44b6427
ROHIT GUPTA <rohitgupta@ROHITs-MacBook-Pro-2.local>
2017-10-10 19:16:03 -0700

    added results component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import QuestionList from "./quiz/QuestionList.jsx";
 import Scorebox from "./quiz/Scorebox.jsx";
+import Results from "./quiz/Results.jsx";
 
 class App extends Component {
 	constructor(props) {
@@ -99,14 +100,22 @@ class App extends Component {
 	}
 
 	render() {
+		if (this.state.current > this.state.questions.length) {
+			var scorebox = "";
+			var results = <Results {...this.state} />;
+		} else {
+			var scorebox = <Scorebox {...this.state} />;
+			var results = "";
+		}
 		return (
 			<div>
-				<Scorebox {...this.state} />
+				{scorebox}
 				<QuestionList
 					{...this.state}
 					setCurrent={this.setCurrent.bind(this)}
 					setScore={this.setScore.bind(this)}
 				/>
+				{results}
 			</div>
 		);
 	}
